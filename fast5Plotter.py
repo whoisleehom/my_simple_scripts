e7889cf5017da45b1a4499dc1ebfb5c2036d8065
whoisleehom <whoisleehom@163.com>
2021-06-04 11:46:41 +0800

    linux multiprocess, file limit 1000

--- fast5Plotter.py
+++ fast5Plotter.py
@@ -3,8 +3,10 @@ import math
 import numpy as np
 import h5py as h5
 import argparse
+import matplotlib
 import matplotlib.pyplot as plt
 import multiprocessing as mp
+matplotlib.use('Agg')
 
 __author__ = 'Yuan GAO'
 __organization__ = 'AGIS'
@@ -12,19 +14,23 @@ __doc__ = ('A script for drawing current intensity from ONT fast5 file.')
 
 
 def getFileBatch(work_dir:str,batch_size:int,name_head:str):
-
-    files = os.listdir(work_dir)
+    files = os.scandir(work_dir)
+    file_count = 0
     file_info = []
     for file in files:#read fast5 file and exract certain info
-        if file[-5:] == 'fast5':
-            read_len = list(h5.File(file,'r')['/Raw/Reads'].values())[0].attrs['duration']
-            file_info.append((file,read_len))
-    
+        if file.name[-5:] == 'fast5':
+            if file_count < 1000:
+                read_len = list(h5.File(file.path,'r')['/Raw/Reads'].values())[0].attrs['duration']
+                file_info.append((file.path,read_len))
+                file_count += 1
+            else:
+                break
+
     file_info=sorted(file_info,key=lambda a:a[1])
     batch_list = []
     batch_count = 1
     for i in range(0,len(file_info),batch_size):
-        batch_list.append((file_info[i:i+batch_size],batch_count,work_dir,name_head))
+        batch_list.append((file_info[i:i+batch_size],batch_count,name_head))
         batch_count += 1
     return batch_list
 
@@ -32,12 +38,11 @@ def getFileBatch(work_dir:str,batch_size:int,name_head:str):
 def drawBatch(batch:list):
     batch_size = len(batch[0])
     batch_count = batch[1]
-    work_dir = batch[2]
-    name_head = batch[3]
+    name_head = batch[2]
 
     raw_data_list = []
     for file in batch[0]:#read fast5 file and exract certain info
-        raw_data = list(h5.File(work_dir + '/' + file[0],'r')['/Raw/Reads'].values())[0]
+        raw_data = list(h5.File(file[0],'r')['/Raw/Reads'].values())[0]
         read_id  = raw_data.attrs['read_id'].decode('utf_8')
         read_len = raw_data.attrs['duration']
         raw_signal = np.asarray(raw_data[('Signal')])
@@ -51,7 +56,7 @@ def drawBatch(batch:list):
 
     for j in range(len(raw_data_list)):
         pic = fig.add_subplot(batch_size,1,j+1)
-        pic.set_title('read ID: {0}; raw data length: {1} + {2}'.format(raw_data_list[j][0],raw_data_list[j][1],max_len-raw_data_list[j][1]),fontsize=8)
+        pic.set_title('read ID: {0}; raw data length: {1} + {2}'.format(raw_data_list[j][0],raw_data_list[j][1],max_len-raw_data_list[j][1]),fontsize=math.floor(7*10/batch_size +1))
         #pic.set_ylabel('current intensity',fontsize=8)
         raw_np = raw_data_list[j][2]
         add_value = raw_np[-10:].mean()
