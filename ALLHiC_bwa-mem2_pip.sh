57cdd3affdacd6580c79991eb9d53ac8ab6786dc
whoisleehom <whoisleehom@163.com>
2021-06-07 14:57:23 +0800

    a script for running allhic quicker

--- ALLHiC_bwa-mem2_pip.sh
+++ ALLHiC_bwa-mem2_pip.sh
@@ -12,7 +12,7 @@ usage()
 	exit 0
 }
 ### edit your bwa-mem2 path ###
-export BWAMEM2=/public1/home/stu_gaoyuan/software/bwa-mem2-2.2.1_x64-linux/bwa-mem2
+export BWAMEM2='/share/home/stu_gaoyuan/software/bwa-mem2-2.2.1_x64-linux/bwa-mem2'
 ### get options
 while getopts ':r:1:2:k:e:t:b:' OPT; do
 	case $OPT in
@@ -55,32 +55,48 @@ fi
 if [ $enzyme = MBOI ]; then
 	enzyme=GATC
 fi
-### link required files
+### step_1 link required files
 ln -s ${ref} ./seq.fasta
 ln -s ${R1} ./Lib_R1.fastq.gz
 ln -s ${R2} ./Lib_R2.fastq.gz
-### 1st round of mapping
+### step_2 1st round of mapping
 $BWAMEM2 index seq.fasta
 samtools faidx seq.fasta
-$BWAMEM2 mem -M -t $threads -K 10000000 seq.fasta Lib_R1.fastq.gz Lib_R2.fastq.gz -o temp1.sam
-samtools sort --reference seq.fasta -O bam -o sorted.bam -@ $threads temp1.sam
+$BWAMEM2 mem -M -t $threads -K 10000000 seq.fasta Lib_R1.fastq.gz Lib_R2.fastq.gz | samtools sort --reference seq.fasta -O bam -o sorted.bam -@ $threads -
+### step_3 uncorrected assembly
+mkdir uncorrected_assembly;cd uncorrected_assembly
+ln -s ../sorted.bam
+ln -s ../seq.fasta* ./ 
+ALLHiC_partition -r seq.fasta -e $enzyme -k $group_count -b sorted.bam
+for ((I=1;I<=$group_count;I++));do j=$(printf "%02s" $I | tr " " "0");mv sorted.counts_GATC.${group_count}g${I}.txt group${j}.txt;echo "allhic optimize group${j}.txt sorted.clm" >> cmd.list;done
+ParaFly -c cmd.list -CPU $threads
+while [[ -e FailedCommands ]]; do rm cmd.list; rm cmd.list.completed; mv FailedCommands cmd.list; ParaFly -c cmd.list -CPU $threads; done
+rm cmd.list; rm cmd.list.completed
+### step_4 first build and plot
+ALLHiC_build seq.fasta
+perl ~/scripts/getFaLen.pl -i groups.asm.fasta -o len.txt
+grep group len.txt > chrn.list
+ALLHiC_plot sorted.bam groups.agp chrn.list $bin_size pdf
+cd ..
+### step_5 correct contig
 samtools index sorted.bam
-### correct contig
 ALLHiC_corrector -m sorted.bam -r seq.fasta -o seq.HiCcorrected.fasta -t $threads
-### 2nd round of mapping
+### step_6 2nd round of mapping
 $BWAMEM2 index seq.HiCcorrected.fasta
 samtools faidx seq.HiCcorrected.fasta
-$BWAMEM2 mem -M -t $threads -K 10000000 seq.HiCcorrected.fasta Lib_R1.fastq.gz Lib_R2.fastq.gz -o temp2.sam
-samtools sort --reference seq.HiCcorrected.fasta -O bam -o sample.bwa_mem.bam -@ $threads temp2.sam
-### partition
+$BWAMEM2 mem -M -t $threads -K 10000000 seq.HiCcorrected.fasta Lib_R1.fastq.gz Lib_R2.fastq.gz | samtools sort --reference seq.HiCcorrected.fasta -O bam -o sample.bwa_mem.bam -@ $threads -
+### step_7 assembly
+mkdir assembly;cd assembly
+ln -s ../seq.HiCcorrected.fasta* ./
+ln -s ../sample.bwa_mem.bam
 ALLHiC_partition -r seq.HiCcorrected.fasta -e $enzyme -k $group_count -b sample.bwa_mem.bam
-### optimize
-rm cmd.list
-for((K=1;K<=$group_count;K++));do echo "allhic optimize sample.bwa_mem.counts_GATC.${group_count}g${K}.txt sample.bwa_mem.clm" >> cmd.list;done
-ParaFly -c cmd.list -CPU $group_count
-### build
+for ((I=1;I<=$group_count;I++));do j=$(printf "%02s" $I | tr " " "0");mv sample.bwa_mem.counts_GATC.${group_count}g${I}.txt group${j}.txt;echo "allhic optimize group${j}.txt sample.bwa_mem.clm" >> cmd.list;done
+ParaFly -c cmd.list -CPU $threads
+while [[ -e FailedCommands ]]; do rm cmd.list; rm cmd.list.completed; mv FailedCommands cmd.list; ParaFly -c cmd.list -CPU $threads; done
+rm cmd.list; rm cmd.list.completed
+### step_8 second build and plot
 ALLHiC_build seq.HiCcorrected.fasta
-### plot
 perl ~/scripts/getFaLen.pl -i groups.asm.fasta -o len.txt
-grep sample len.txt > chrn.list
+grep group len.txt > chrn.list
 ALLHiC_plot sample.bwa_mem.bam groups.agp chrn.list $bin_size pdf
+cd ..
